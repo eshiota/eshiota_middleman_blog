1879462ba82015c6976adef5f8d7cc8da37238cd
Eduardo Shiota Yasuda <yl.edu.sy@gmail.com>
2013-07-01 00:57:44 -0300

    Add portfolio section

--- source/javascripts/app/bootstrap.js
+++ source/javascripts/app/bootstrap.js
@@ -1,4 +1,8 @@
 (function () {
+  var portfolioLists = $$(".portfolio-list")
+    , masonryLists = []
+  ;
+
   Instafetcher.getUserMedia({
     access_token : "896055.0250db2.7bf9102e46774f2ebefc6570148a0363",
     user_id      : "896055",
@@ -8,4 +12,16 @@
   FitVids({
     element : "#blog-content"
   });
+
+  portfolioLists.forEach(function (el, index, array) {
+    masonryLists.push(new Masonry(el));
+  });
+
+  // Masonry is not playing nice calculating the heights, so I manually
+  // trigger a recalc here
+  setTimeout(function () {
+    masonryLists.forEach(function (el) {
+      el.layout();
+    });
+  }, 100);
 })();
--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require vendor/prism
 //= require vendor/matchmedia
 //= require vendor/picturefill
+//= require vendor/masonry
 //= require app/selectors
 //= require app/extend
 //= require app/fitvids
